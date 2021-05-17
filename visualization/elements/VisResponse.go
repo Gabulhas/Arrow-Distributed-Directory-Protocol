1f7475e855ce14c7e0d3621265bb4433b6949336
Guilherme Lopes <Gabulhas@users.noreply.github.com>
2021-05-17 11:58:45 +0100

    Versão Final

--- visualization/elements/VisResponse.go
+++ visualization/elements/VisResponse.go
@@ -4,5 +4,4 @@ type VisResponse struct {
 	Nodes     []Node       `json:"nodes"`
 	Links     []Connection `json:"links"`
 	QueueCons []Connection `json:"queue_cons"`
-	//QueueResponse QueueResponse `json:"queue_response"`
 }
